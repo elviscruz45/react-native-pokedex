05f33756155958a71acc5733311df8011c434da8
Elvis <elviscruz45@gmail.com>
2023-01-10 20:12:57 -0500

    finished

---
+++ src/screens/Pokemon.js
@@ -0,0 +1,40 @@
+import { ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { getPokemonDetailsApi } from "../api/pokemon";
+import Header from "../components/Pokemon/Header";
+import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
+
+export default function Pokemonsss(props) {
+  const {
+    navigation,
+    route: { params },
+  } = props;
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if (!pokemon) return null;
+
+  return (
+    <ScrollView>
+      <Header
+        name={pokemon.name}
+        order={pokemon.order}
+        image={pokemon.sprites.other["official-artwork"].front_default}
+        type={pokemon.types[0].type.name}
+      />
+      <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
+    </ScrollView>
+  );
+}
